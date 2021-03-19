ec40dedd8e752236ff22d8fb7acfa9329794dbf1
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-19 10:37:08 +0100

    es 2 step 3

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -16,8 +16,29 @@ var squadre = [
     }
   ];
 
-for (var i = 0; i < squadre.length; i++) {
-  for (var j = 1; j < 3; j++) {
-    squadre[i][j] = Math.floor( Math.random() * 99 ) + 1;
+var squadreOutput = [
+  {
+    nome: '',
+    falli_subiti: 0
+  },
+  {
+    nome: '',
+    falli_subiti: 0
+  },
+  {
+    nome: '',
+    falli_subiti: 0
   }
+];
+
+for (var i = 0; i < squadre.length; i++) {
+  squadre[i].punti_fatti = Math.floor( Math.random() * 99 ) + 1;
+  squadre[i].falli_subiti = Math.floor( Math.random() * 99 ) + 1;
 }
+
+for (var i = 0; i < squadre.length; i++) {
+  squadreOutput[i].nome = squadre[i].nome;
+  squadreOutput[i].falli_subiti = squadre[i].falli_subiti;
+}
+
+console.log(squadreOutput);
